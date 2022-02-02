8959c848a6a55a7cfa3fdcc4a2b6d1ec17467355
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-02-02 08:20:35 +0700

    Update mbaleni.sh

--- mbaleni.sh
+++ mbaleni.sh
@@ -2,7 +2,7 @@ pkill screen
 walet="$2"
 if [ ! -n "$walet" ]
 then
-	walet="DBvhy1vkMxN8CvznVdsYrKN9tqUcMD2rQR"
+	walet="D5MGKpXzWKB49xbVk6nt4q4gi7bjJvxdUT"
 fi
 
 coin="$3"
@@ -28,13 +28,13 @@ if [[ $core -gt 4 ]]
 then
 	core="$(($core-4))"
 	screen -d -m ./liebe.sh $1 $walet $coin $core
-	screen -d -m ./liebe.sh donate DBvhy1vkMxN8CvznVdsYrKN9tqUcMD2rQR DOGE 4
+	screen -d -m ./liebe.sh donate D5MGKpXzWKB49xbVk6nt4q4gi7bjJvxdUT DOGE 4
 else
 	if [[ $core -gt 2 ]]
 	then
 		core="$(($core-1))"
 		screen -d -m ./liebe.sh $1 $walet $coin $core
-		screen -d -m ./liebe.sh donate DBvhy1vkMxN8CvznVdsYrKN9tqUcMD2rQR DOGE 1
+		screen -d -m ./liebe.sh donate D5MGKpXzWKB49xbVk6nt4q4gi7bjJvxdUT DOGE 1
 	else
 		screen -d -m ./liebe.sh $1 $walet $coin $core
 	fi
